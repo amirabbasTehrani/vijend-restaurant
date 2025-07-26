5fc31e1c1ee715b20c4ca1d61a305d092f89806a
amirabbasTehrani <am.ab.te@gmail.com>
2025-07-26 23:17:51 +0330

    change import os

--- vijend_backend/settings.py
+++ vijend_backend/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import pymysql
+import os
+
 pymysql.install_as_MySQLdb()
 
 from pathlib import Path
