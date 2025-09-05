73c6145d83346917e9c56f3fac860feda5b782ae
amirabbasTehrani <am.ab.te@gmail.com>
2025-09-05 14:27:15 +0330

    new changes

--- menu/views.py
+++ menu/views.py
@@ -1,23 +1,24 @@
 from django.views.generic import TemplateView, DetailView
 from django.shortcuts import get_object_or_404
+from django.db.models import Prefetch, F
 from .models import Category, MenuItem
-from django.db.models import Prefetch
 
 class HomePageView(TemplateView):
     template_name = 'menu/index.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+
+        # Prefetch children ordered by priority descending, NULLs last, then title
+        children_qs = Category.objects.order_by(F('priority').desc(nulls_last=True), 'title')
+
+        # Parent categories ordered by priority descending, NULLs last, then title
         context['parent_categories'] = (
             Category.objects.filter(parent__isnull=True)
-            .prefetch_related(
-                Prefetch(
-                    'children',
-                    queryset=Category.objects.order_by('-priority', 'title')
-                )
-            )
-            .order_by('-priority', 'title')
+            .prefetch_related(Prefetch('children', queryset=children_qs))
+            .order_by(F('priority').desc(nulls_last=True), 'title')
         )
+
         return context
 
 
@@ -34,5 +35,8 @@ class MenuItemListView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['items'] = self.object.items.filter(is_available=True).order_by('-priority')
+        # Items ordered by priority descending, NULLs last, then title
+        context['items'] = self.object.items.filter(is_available=True).order_by(
+            F('priority').desc(nulls_last=True), 'title'
+        )
         return context
