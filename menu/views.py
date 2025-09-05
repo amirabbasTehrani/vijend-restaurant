37c86ab72cd65d9b10d387a629373573b6aac420
amirabbasTehrani <am.ab.te@gmail.com>
2025-09-05 14:06:27 +0330

    new changes

--- menu/views.py
+++ menu/views.py
@@ -10,8 +10,10 @@ class HomePageView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['parent_categories'] = (
             Category.objects.filter(parent__isnull=True)
-            .prefetch_related('children')
-            .order_by('-priority')
+        .prefetch_related(
+            Prefetch('children', queryset=Category.objects.order_by('-priority'))
+        )
+        .order_by('-priority')
         )
         return context
 
